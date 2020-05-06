7cc106b131aa4bad57ba7d33085c7d3ff864781f
FacuCuzziol <facucuzziol@gmail.com>
2020-05-06 16:35:21 -0300

    adding contents in mydbcontext.cs

--- Actividad3-DACS/Actividad3-DACS/Models/MyDbContext.cs
+++ Actividad3-DACS/Actividad3-DACS/Models/MyDbContext.cs
@@ -5,7 +5,14 @@ using System.Web;
 
 namespace Actividad3_DACS.Models
 {
-    public class MyDbContext
+    public class MyDbContext : DbContext
     {
+    public MyDbContext()
+        :base(nameOrConnectionString: "<add connectionString=”Server=(local);Database=MyDbContext;Trusted_Connection=True;MultipleActiveResultSets=true” name=”MyDbContext” providerName=”System.Data.SqlClient”/>")
+    {}
+
+    public DbSet<Client> Clients { get; set;}
+
+    public DbSet<Sale> Sales { get; set;}
     }
-}
+    }
