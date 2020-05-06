0395bd0d04957786edbc7bb705a410a2af9b6838
AnastaciusWright <alexandronadal@gmail.com>
2020-05-06 16:04:39 -0300

    UnitOfWork

---
+++ Actividad3-DACS/Actividad3-DACS/Repositories/UnitOfWork.cs
@@ -0,0 +1,39 @@
+public class UnitOfWork
+{
+    public UnitOfWork()
+    {
+        this.context = new MyDbContext();
+    }
+    private readonly MyDbContext context;
+ 
+    private GenericRepository<Client> clientsRepository;
+    public GenericRepository<Client> ClientsRepository
+    {
+        get
+        {
+            if (this.clientsRepository == null)
+            {
+                this.clientsRepository = new GenericRepository<Client>(this.context);
+            }
+            return this.clientsRepository;
+        }
+    }
+ 
+    private GenericRepository<Sale> salesRepository;
+    public GenericRepository<Sale> SalesRepository
+    {
+        get
+        {
+            if (this.salesRepository == null)
+            {
+                this.salesRepository = new GenericRepository<Sale>(this.context);
+            }
+            return this.salesRepository;
+        }
+    }
+ 
+    public async Task SaveChangesAsync()
+    {
+        await this.context.SaveChangesAsync();
+    }
+}
