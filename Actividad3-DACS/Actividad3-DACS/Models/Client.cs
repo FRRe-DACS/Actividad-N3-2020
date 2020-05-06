406cb6621598ea1a3b1d9d481ddb2d8eca1bfbf5
Juan Cruz Soto <juancruzlp2013@gmail.com>
2020-05-06 16:03:31 -0300

    Cliente modificado

--- Actividad3-DACS/Actividad3-DACS/Models/Client.cs
+++ Actividad3-DACS/Actividad3-DACS/Models/Client.cs
@@ -7,8 +7,27 @@ namespace Actividad3_DACS.Models
 {
     public class Client
     {
-
-
+	/// <summary>
+	///
+	/// </summary>
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	public int ClientId {get; set; }
+	/// <summary>
+	///
+	/// </summary>
+	[Required]
+	public string FullName {get; set; }
+	/// <summary>
+	///
+	/// </summary>
+	[DataType(DataType.EmailAddress)]
+	public string Email { get; set; }
+	/// <summary>
+	///
+	/// </summary>
+	[DataType(DataType.PhoneNumber)]
+	public string PhoneNumber { get; set; }
 
     }
 }
